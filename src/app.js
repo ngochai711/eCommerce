c69152a3addc4307060748cf8239a120e138a456
NGUYEN HOANG NGOC HAI <>
2025-01-15 22:45:14 +0700

    Section 5. Add sign-up, create token and use lodash to shorted respond info

--- src/app.js
+++ src/app.js
@@ -10,18 +10,14 @@ const app = express()
 app.use(morgan("dev"))
 app.use(helmet())
 app.use(compression())
-
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 // init db
 require('./dbs/init.mongodb')
 
-// init routes
-app.get('/', (req, res, next) =>{
-    const strCompress = "Hello Hint"
-    return res.status(200).json({
-        message:'Welcome Hint',
-        metadata: strCompress.repeat(10000)
-    })
-})
+app.use('/', require('./routes'))
 // handling error
 
 module.exports = app;
---
+++ src/services/access.service.js
@@ -0,0 +1,93 @@
+'use strict'
+
+const shopModel = require("../models/shop.model")
+const bcrypt = require('bcrypt')
+const crypto = require('crypto')
+const KeyTokenService = require("./keyToken.service")
+const { createTokenPair } = require("../auth/authUtils")
+const { getInfoData } = require('../utils/index')
+
+const RoleShop = {
+    SHOP: 'SHOP',
+    WRITER: 'WRITER',
+    EDITOR: 'EDITOR',
+    ADMIN: 'ADMIN'
+}
+
+class AccessService {
+    
+    static signUp = async ({ name, email, password}) => {
+        try {
+            // 1. check email exists?
+            const holderShop = await shopModel.findOne({email}).lean() // lean giảm tải sign context
+            if(holderShop){
+                return {
+                    code: 'xxxx',
+                    message: 'Shop already registered!'
+                }
+            }
+
+
+            const passswordHash = await bcrypt.hash(password, 10)
+            const newShop = await shopModel.create({
+                name, email, password: passswordHash, roles: [RoleShop.SHOP]
+            })
+
+            if(newShop){
+                // create privateKey, publicKey
+                // const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa',{
+                //     modulusLength: 4096,
+                //     publicKeyEncoding:{
+                //         type: 'pkcs1',
+                //         format: 'pem'
+                //     },
+                //     privateKeyEncoding:{
+                //         type: 'pkcs1',
+                //         format: 'pem'
+                //     }
+                // })
+
+                const privateKey = crypto.randomBytes(64).toString('hex')
+                const publicKey = crypto.randomBytes(64).toString('hex')
+
+                const keyStore = await KeyTokenService.createKeyToken({
+                    userId: newShop._id,
+                    publicKey,
+                    privateKey
+                })
+
+                if(!keyStore){
+                    return {
+                        code: 'xxxx',
+                        message: 'keyStore error'
+                    }
+                }
+
+                // create token pair
+                const tokens = await createTokenPair({userId: newShop._id, email}, publicKey, privateKey)
+
+                return {
+                    code: 201,
+                    metadata:{
+                        shop: getInfoData({ fields: ['_id','name','email'], object: newShop}),
+                        tokens
+                    }
+                }
+                
+            }
+
+            return {
+                code: 200,
+                metadata: null
+            }
+        } catch (error) {
+            return {
+                code: 'xxx', // Define trong docs mã xxx là lỗi gì 
+                message: error.message, // Show lỗi ra
+                status: 'error' // Status để biết request này bị lỗi
+            }
+        }
+    }
+}
+
+module.exports = AccessService
