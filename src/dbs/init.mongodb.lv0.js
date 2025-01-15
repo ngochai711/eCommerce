04ad5727d84c5ed7032309907d64f9e358e35c4f
NGUYEN HOANG NGOC HAI <>
2025-01-15 08:37:51 +0700

    Section 4: Explain .env file and configs, way to set up config from local to cloud

--- src/dbs/init.mongodb.lv0.js
+++ src/dbs/init.mongodb.lv0.js
@@ -4,15 +4,7 @@ const mongoose  = require("mongoose")
 
 const connectString = `mongodb://localhost:27017/shopDEV`
 
-class Database {
-    constructor(){
-        this.connect()
-    }
+mongoose.connect(connectString).then(_ => console.log(`Connected Mongodb Success`))
+.catch( err => console.log(`Error Connect!`))
 
-    //connect
-    connect(type = 'mongodb'){
-        mongoose.connect(connectString).then(_ => console.log(`Connected Mongodb Success`))
-        .catch( err => console.log(`Error Connect!`))
-    }
-}
 module.exports = mongoose;
